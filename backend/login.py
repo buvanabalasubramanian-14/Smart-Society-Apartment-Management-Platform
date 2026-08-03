1f4d6275a613a5909f026f8ba9df42b6e748e545
Buvana <buvanabalasubramanian14@gmail.com>
2026-08-03 22:16:28 +0530

    Completed login module

--- backend/login.py
+++ backend/login.py
@@ -1,5 +1,18 @@
 def login():
-    username = input("Enter Username: ")
+
+    print("\n===== Login =====")
+    email = input("Enter Email: ")
     password = input("Enter Password: ")
-    print("\nLogin Successful")
-    print("Welcome,", username)
+    try:
+        file = open("../database/users.txt", "r")
+        for line in file:
+            user = line.strip().split(",")
+            if user[3] == email and user[4] == password:
+                print("\nLogin Successful!")
+                print("Welcome", user[0])
+                file.close()
+                return
+        file.close()
+        print("\nInvalid Email or Password")
+    except:
+        print("\nNo users found.")
